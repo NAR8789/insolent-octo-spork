12fc7aaf16563806052ea0d9336241c18e4f1b91
xiao.fan <xiao.fan@workday.com>
2015-09-24 15:51:09 -0700

    fix name of attached thumbnail

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -12,7 +12,7 @@
 
 class Thumbnail < ActiveRecord::Base
   belongs_to :comic
-  has_attached_file :avatar
+  has_attached_file :thumbnail
 
   validates :comic, presence: true
   validates_attachment :thumbnail, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }

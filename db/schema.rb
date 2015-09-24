ee10cb16e3890dc1f30d8f166d6ae8e21d630d14
xiao.fan <xiao.fan@workday.com>
2015-09-24 15:02:46 -0700

    create user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924211916) do
+ActiveRecord::Schema.define(version: 20150924215829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20150924211916) do
   end
 
   add_index "thumbnails", ["comic_id"], name: "index_thumbnails_on_comic_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   add_foreign_key "comics", "artists"
   add_foreign_key "thumbnails", "comics"

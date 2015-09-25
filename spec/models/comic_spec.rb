3b4d64cc1aa94933b13b3875a06648d0655f55ce
xiao.fan <xiao.fan@workday.com>
2015-09-24 17:41:00 -0700

    missing annotation

--- spec/models/comic_spec.rb
+++ spec/models/comic_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: comics
+#
+#  id                  :integer          not null, primary key
+#  name                :string           not null
+#  link                :string
+#  default_description :text
+#  artist_id           :integer
+#
+
 require 'rails_helper'
 
 RSpec.describe Comic, type: :model do

76278b01a2924858f54813305d1329b8f29ff828
xiao.fan <xiao.fan@workday.com>
2015-09-24 22:32:48 -0700

    make thumbnails visible and submittable

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -1,20 +1,27 @@
 class ThumbnailsController < ApplicationController
+  before_action :set_comic
+  before_action :set_thumbnail, only: [:show, :destroy]
+
+  def show
+  end
+
   def create
-    @thumbnail = Thumbnail.new(thumbnail_params)
+    @thumbnail = @comic.thumbnails.new(thumbnail_params)
 
     respond_to do |format|
       if @thumbnail.save
         format.html { redirect_to @thumbnail, notice: 'Thumbnail was successfully created.' }
-        format.json { render :show, status: :created, location: @thumbnail }
+        format.js { render json: @thumbnail }
+        format.json { render json: @thumbnail }
       else
         format.html { render :new }
+        format.js { render json: @thumbnail.errors, status: :unprocessable_entity }
         format.json { render json: @thumbnail.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    @thumbnail = Thumbnail.find(params[:id])
     @thumbnail.destroy
     respond_to do |format|
       format.html { redirect_to thumbnails_url, notice: 'Thumbnail was successfully destroyed.' }
@@ -23,7 +30,15 @@ class ThumbnailsController < ApplicationController
   end
 
   private
+    def set_comic
+      @comic = Comic.find(params[:comic_id])
+    end
+
+    def set_thumbnail
+      @thumbnail = Thumbnail.find(params[:id])
+    end
+
     def thumbnail_params
-      params[:thumbnail]
+      params.require(:thumbnail).permit(:thumbnail)
     end
 end
